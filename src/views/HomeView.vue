5fc11c9193705b954b5a45bd5f59b07bbf5088b8
Per Olsen <persteen22@gmail.com>
2026-07-13 21:21:34 +0200

    Updated in home vue to - Refresh token Reuse ...

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,7 +15,7 @@ usersStore.getAll();
 <template>
     <div>
         <h1>Hi {{authUser?.username}}!</h1>
-        <p>You're logged in by Vue 3 using JWT with Refresh Token Rotation and Revoked Token Reuse Detection</p>
+        <p>You're logged in by Vue 3 using JWT with Refresh Token Rotation and Refresh Token Reuse Detection</p>
         <h3>Users served from a secure api endpoint:</h3>
         <ul v-if="users.length">
             <li v-for="user in users" :key="user.id">{{user.username}} - {{user.name}}</li>
